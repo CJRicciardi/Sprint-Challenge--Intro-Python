c40871af631535ba44ac0a7652b84b142f12855e
CJ Ricciardi <ricciardistg@gmail.com>
2020-06-07 10:57:24 -0400

    SPrint Challenge completed in 2.5 hours.

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -1,7 +1,23 @@
+import pandas as pd
+
 # Create a class to hold a city location. Call the class "City". It should have
 # fields for name, lat and lon (representing latitude and longitude).
 
+class City():
+  def __init__(self, name, lat, lon):
+    self.name = name
+    self.lat = lat
+    self.lon = lon
 
+  def __str__(self):
+    return f'{self.name}, {self.lat}, {self.lon}'
+
+# kenosha = City('Kenosha', 500, 1000)
+
+# print(kenosha)
+
+# quit()
+   
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
 #
@@ -14,14 +30,30 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+df = pd.read_csv('cities.csv')
+
+cities_list = df['city'].tolist()
+
+lats = df['lat'].tolist()
+
+lons = df['lng'].tolist()
+
+# print(cities_list[0:5])
+
+# print(lats[0:5]),
+
+# print(lons[0:5])
+
+# quit()
+
+
 cities = []
 
 def cityreader(cities=[]):
-  # TODO Implement the functionality to read from the 'cities.csv' file
-  # For each city record, create a new City instance and add it to the 
-  # `cities` list
-    
-    return cities
+
+  for city, lat, lon in zip(cities_list, lats, lons):
+    cities.append(City(city, lat, lon))
+  return cities
 
 cityreader(cities)
 
--- src/comp/comp.py
+++ src/comp/comp.py
@@ -21,51 +21,65 @@ humans = [
     Human("David", 31),
 ]
 
+
+
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with 'D':
 print("Starts with D:")
-a = []
+a = [d.name for d in humans if d.name[0] == 'D']
 print(a)
+
+# quit()
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name ends in "e".
 print("Ends with e:")
-b = []
+b = [d.name for d in humans if d.name[-1] == 'e']
 print(b)
+
+# quit()
 
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = []
+c = [d.name for d in humans if (d.name[0] == 'C') or (d.name[0] == 'D') or (d.name[0] == 'E') or (d.name[0] == 'F') or (d.name[0] == 'G')]
 print(c)
+
+# quit()
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = []
+d = [h.age + 10 for h in humans]
 print(d)
+
+# quit()
 
 # Write a list comprehension that creates a list of strings which are the name
 # joined to the age with a hyphen, for example "David-31", for all humans.
 print("Name hyphen age:")
-e = []
+e = [f"{h.name}-{h.age}" for h in humans]
 print(e)
+
+# quit()
 
 # Write a list comprehension that creates a list of tuples containing name and
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = []
+f = [(h.name, h.age) for h in humans if (h.age >= 27) and (h.age <= 32)]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
 # list, except with all the names uppercase and the ages with 5 added to them.
 # The "humans" list should be unmodified.
 print("All names uppercase:")
-g = []
+name = [h.name.upper() for h in humans]
+age = [h.age + 5 for h in humans]
+g = [Human(name, age) for name, age in zip(name, age)]
 print(g)
 
 # Write a list comprehension that contains the square root of all the ages.
 print("Square root of ages:")
 import math
-h = []
+h = [math.sqrt(h.age) for h in humans]
 print(h)
